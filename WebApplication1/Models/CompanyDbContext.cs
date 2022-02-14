9f788d584b43ff510d8fe030c7ba24b357c9d135
nnanhtu0107 <nnanhtu017@gmail.com>
2022-02-14 19:49:47 +0700

    EAP

---
+++ WebApplication1/Models/CompanyDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace WebApplication1.Models
+{
+    public class CompanyDbContext : DbContext
+    {
+        public CompanyDbContext() : base("name=DefaultConnection"){ }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
+
